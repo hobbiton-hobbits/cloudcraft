d9ad0a03b9095e589c7fece4efd387de2f2a41d2
Brian Vose <103958999+Banzubie@users.noreply.github.com>
2022-09-30 17:28:53 -0600

    forgot to add self/chat messages

---
+++ client/src/components/CurrentChat/ChatMessageOther.jsx
@@ -0,0 +1,17 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
+const ChatMessageOther = () => {
+  return (
+    <div className='current-chat-message-other' >
+      <div>
+        <div>
+
+        </div>
+      </div>
+    </div>
+  )
+}
+
+
+export default ChatMessageOther;
