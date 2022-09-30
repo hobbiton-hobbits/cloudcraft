00dec78c4d1c16f485ceb6629367125d5bdc5e2e
James Anderson <jamespanderson22@gmail.com>
2022-09-30 16:01:06 -0400

    "Added Div for testing"

--- client/src/components/TaskList/TaskList.jsx
+++ client/src/components/TaskList/TaskList.jsx
@@ -1,16 +1,17 @@
 import React from "react";
 
 const TaskList = (props) => {
-    return (
-      <div id='task-list' className='widget'>
-        <div className='widget-title'>Tasks</div>
-        <div className='task-list-task'>Task 1</div>
-        <div className='task-list-task'>Task 2</div>
-        <div className='task-list-task'>Task 3</div>
-        <div className='task-list-task'>Task 4</div>
-        <div className='task-list-task'>Task 5</div>
-      </div>
-    )
-  }
+  return (
+    <div id="task-list" className="widget">
+      <div className="widget-title">Tasks</div>
+      <div className="task-list-task">Task 1</div>
+      <div className="task-list-task">Task 2</div>
+      <div className="task-list-task">Task 3</div>
+      <div className="task-list-task">Task 4</div>
+      <div className="task-list-task">Task 5</div>
+      <div className="task-list-task">Task 6</div>
+    </div>
+  );
+};
 
-  export default TaskList;
+export default TaskList;
