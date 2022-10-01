7d18368e35ede1a2784755754a7aba3a896f7acc
ytang <107004276+yota88@users.noreply.github.com>
2022-09-30 20:33:17 -0400

    Added socket connections to client and server. Added login component.

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,10 +1,50 @@
 import React from "react";
+import { useState, useEffect } from 'react';
+import io from 'socket.io-client';
 import UserList from "./UserList/UserList.jsx";
 import GroupList from "./GroupList/GroupList.jsx";
 import CurrentChat from "./CurrentChat/CurrentChat.jsx";
 import TaskList from "./TaskList/TaskList.jsx";
 
+const socket = io.connect('http://localhost:8085');
+
 const App = () => {
+ // Change if using Recoil state manager
+ const [loggedIn, setLoggedIn] = useState(false);
+ const [fullName, setFullName] = useState(null);
+ const [username, setUsername] = useState(null);
+ const [recipient, setRecipient] = useState(null);
+ const [group, setGroup] = useState('general');
+ const [receivedMsg, setReceivedMsg] = useState(null);
+
+
+ useEffect(() => {
+   // Once login is implemented, uncomment out if statement
+   // if (loggedIn) {
+     socket.on('welcome-back', (socketID) => {
+       // Confirm socketID connection with server
+       console.log(`Welcome back: ${socketID}`);
+       // Send default recipient and group
+       socket.emit('join-group', {
+         username,
+         recipient,
+         group,
+       }, (response) => console.log('Joined general room'));
+     });
+
+     socket.on('receive-msg', (messages) => {
+       // data may be in different data structure;
+       setReceivedMsg(messages);
+       console.log('Messages received:', messages);
+     });
+   // }
+ }, [socket])
+
+ // Implement conditional rendering of login page once finished with Auth
+ // if (!loggedIn) {
+ //   return <Login/>
+ // }
+
   return (
     <div>
       <div id='page-title'>cloudcraft</div>
---
+++ client/src/components/Login/Login.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Login = (props) => {
+  return (
+    <div id="login" className="widget"></div>
+  );
+};
+
+export default Login;
