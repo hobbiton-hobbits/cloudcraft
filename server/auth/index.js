4e088350898692f7ace4051593510364bc2250f0
Alex Lee <alexylee15@gmail.com>
2022-10-03 14:54:18 -0400

    JWT authetnication working without refresh token

--- server/auth/index.js
+++ server/auth/index.js
@@ -5,7 +5,7 @@ const compression = require('compression');
 const port = process.env.AUTHPORT;
 const db = require('./authdb');
 const jwt = require('jsonwebtoken');
-
+const bcrypt = require('bcrypt');
 
 const app = express();
 
@@ -33,19 +33,61 @@ const authenticateToken = (req, res, next) => {
   })
 }
 
+app.post('/register', async (req, res) =>{
+  //salt
+  const salt = await bcrypt.genSalt();
+  //hash password
+  const hashedPassword = await bcrypt.hash(req.body.password, salt);
+
+  //create user
+  const user = {username: req.body.username, password: hashedPassword};
+  console.log(user);
+
+  //See if Username is taken
+  await db.query(`Select username from auth where username = '${user.username}';`)
+    .then( async (result) => {
+      if (result.rows.length > 0) {
+        res.status(500).send('Username Already Taken')
+      } else {
+        await db.query(`INSERT INTO auth (username, password) VALUES ('${user.username}', '${user.password}');`)
+          .then((data) => {
+            res.status(201).send('Registered User')
+          })
+          .catch((err) => {
+            res.status(500).send('error creating user')
+          });
+      }
+    })
+    .catch((err) => {
+      res.status(500).send('Error Creating User')
+    });
+})
+
+
+
 app.get('/auth', authenticateToken, (req, res) => {
   res.send(req.user.name + ' is logged in');
 })
 
-app.post('/login', (req, res) => {
-  //AUTHENTICATE USER BY CHECKING DATABASE AND PASSWORD
-  const username = req.body.username;
+
+app.post('/login', async (req, res) => {
   const user = {
-    name: username,
+    username: req.body.username,
+    password: req.body.password
   }
-
-
-  //SEND TOKEN BACK
-  const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET);
-  res.json({accessToken: accessToken});
+  //AUTHENTICATE USER BY CHECKING DATABASE AND PASSWORD
+  await db.query(`SELECT password FROM auth WHERE username = '${user.username}'`)
+    .then(async (result) => {
+      let isTrue = await bcrypt.compare(user.password, result.rows[0].password);
+      if (!isTrue) {
+        res.status(400).send('Incorrect Login Credentials')
+      } else {
+        //SEND TOKEN BACK
+        const accessToken = jwt.sign(user.username, process.env.ACCESS_TOKEN_SECRET);
+        res.status(201).json({accessToken: accessToken});
+      }
+    })
+    .catch((err) => {
+      res.status(400).send('User Not Found');
+    });
 });
