4c9dc425677428938ffc6eb845802f6f159a257d
Alex Lee <alexylee15@gmail.com>
2022-10-03 09:59:36 -0400

    created auth table maker file

---
+++ server/auth/authdb/makeAuthTables.js
@@ -0,0 +1,35 @@
+const {Pool} = require('pg');
+require('dotenv').config();
+
+
+const client = new Pool({
+  host: process.env.AUTHHOST,
+  user: process.env.NAME,
+  port: process.env.PORT,
+  password: process.env.PASSWORD,
+  database: process.env.AUTHDATABASE
+});
+
+// CONNECT TO DATABASE
+client.connect((err) => {
+    if (err) {
+      console.error('error connecting: ' + err.stack);
+      return;
+    }
+    console.log('connected to database');
+});
+
+const makeAuthTable = async () => {
+
+  await client.query(`CREATE TABLE IF NOT EXISTS users (
+    id SERIAL PRIMARY KEY,
+    username TEXT,
+    firstName TEXT,
+    lastName TEXT,
+    token TEXT
+  );`).then(() => console.log('Auth Table Created'))
+  .catch(err => console.log('Table failed to create: ', err))
+  client.end();
+}
+
+makeAuthTable();
